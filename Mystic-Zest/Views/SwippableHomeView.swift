32e7a7832413ab0f5234218ead86862389fb3685
JaiminRaval <siliconvalley.jaimin@gmail.com>
2025-04-15 23:59:50 +0530

    added swiftUI view

---
+++ Mystic-Zest/Views/SwippableHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  SwippableHomeView.swift
+//  Mystic-Zest
+//
+//  Created by Jaimin Raval on 15/04/25.
+//
+
+import SwiftUI
+
+struct SwippableHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwippableHomeView()
+}
