c4f2b2a445e2ba5449fb82fe1b0fea138fd16b6a
JaiminRaval <siliconvalley.jaimin@gmail.com>
2025-04-29 17:38:10 +0530

    removed storyboards
    it's completely blank, pitch-black homeScreen

--- Mystic-Zest/AppDelegate.swift
+++ Mystic-Zest/AppDelegate.swift
@@ -9,11 +9,17 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let homeVC = HomeVC(rootView: SwippableHomeView())
+        window?.rootViewController = homeVC
+        window?.makeKeyAndVisible()
+        
+        
         return true
     }
 
--- Mystic-Zest/ViewModels/HomeVC.swift
+++ Mystic-Zest/ViewModels/HomeVC.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import SwiftUI
 
-class HomeVC: UIViewController {
+class HomeVC: UIHostingController<SwippableHomeView> {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    required init? (coder: NSCoder) {
+        super.init(coder:coder, rootView: SwippableHomeView())
+    }
+    
+    override init(rootView: SwippableHomeView) {
+        super.init(rootView: rootView)
     }
 
 }
+
+
+
+/* Steps to remove storyBoard from UIKit project:
+    1. delete main.storyboard file and their respective VCs
+    2. delete "storyboard name" key-value from info.plist; find it in this path(as of APR 2025): info.plist -> app scene manifest -> scene configuration -> item 0 -> storyboard name
+
+ 
+ 
+ 
+ 
+ */
