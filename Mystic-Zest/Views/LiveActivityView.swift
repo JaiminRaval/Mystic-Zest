3b48937b0662ac8437196628b21055a35b77ca1d
JaiminRaval <siliconvalley.jaimin@gmail.com>
2025-08-03 23:01:49 +0530

    created new view
    added new view for live Activities

---
+++ Mystic-Zest/Views/LiveActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  LiveActivityView.swift
+//  Mystic-Zest
+//
+//  Created by Jaimin Raval on 03/08/25.
+//
+
+import SwiftUI
+
+struct LiveActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LiveActivityView()
+}
