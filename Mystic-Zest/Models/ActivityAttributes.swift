93eb6b15b3e25fbb22ee3dd5fc4b46db69ccc2e0
JaiminRaval <siliconvalley.jaimin@gmail.com>
2025-08-04 23:59:58 +0530

    added model

---
+++ Mystic-Zest/Models/ActivityAttributes.swift
@@ -0,0 +1,23 @@
+//
+//  ActivityAttributes.swift
+//  Mystic-Zest
+//
+//  Created by Jaimin Raval on 04/08/25.
+//
+
+import ActivityKit
+
+struct ZestActivityAttributes: ActivityAttributes {
+    
+//    typealias ContentState =
+
+    public struct contentState: Codable, Hashable {
+        var progress: Double
+        
+    }
+    
+    var title: String
+    
+    
+    
+}
--- Mystic-Zest/Views/LiveActivityView.swift
+++ Mystic-Zest/Views/LiveActivityView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct LiveActivityView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("")
     }
 }
 
